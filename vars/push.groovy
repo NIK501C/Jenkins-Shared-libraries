950657e417ccf78477d08323c75fa51d28c2eed3
NIK501C <154682784+NIK501C@users.noreply.github.com>
2026-05-29 09:36:13 +0530

    Rename function 'come' to 'call'
    Renamed function from 'come' to 'call' for clarity.

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,4 @@
-def come(String img){
+def call(String img){
   echo "This stage is pushing the image on DockerHub"
   withCredentials([usernamePassword("credentialsId":"DockerHubCred", passwordVariable: "DockerHubPass", usernameVariable: "DockerHubUser")]){
   sh "sudo docker login -u ${env.DockerHubUser} -p ${env.DockerHubPass} "
