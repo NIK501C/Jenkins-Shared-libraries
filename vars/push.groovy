0fbd38aed167a268e4410a4ca8ee1fd204800c1c
NIK501C <154682784+NIK501C@users.noreply.github.com>
2026-05-30 10:22:14 +0530

    Fix syntax for withCredentials in push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,6 +1,6 @@
 def call(String img){
   echo "This stage is pushing the image on DockerHub"
-  withCredentials([usernamePassword("credentialsId":"DockerHubCred", passwordVariable: "DockerHubPass", usernameVariable: "DockerHubUser")]){
+  withCredentials([usernamePassword(credentialsId:"DockerHubCred", passwordVariable: "DockerHubPass", usernameVariable: "DockerHubUser")]){
   sh "sudo docker login -u ${DockerHubUser} -p ${DockerHubPass} "
   sh "sudo docker image tag ${img}:latest ${DockerHubUser}/${img}:latest "
   sh "sudo docker push ${DockerHubUser}/${img}:latest"
