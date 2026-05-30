e30dff1838021e37ba59e956cef0197b286e68dd
NIK501C <154682784+NIK501C@users.noreply.github.com>
2026-05-30 10:04:08 +0530

    Fix variable usage in Docker push script

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,9 +1,9 @@
 def call(String img){
   echo "This stage is pushing the image on DockerHub"
   withCredentials([usernamePassword("credentialsId":"DockerHubCred", passwordVariable: "DockerHubPass", usernameVariable: "DockerHubUser")]){
-  sh "sudo docker login -u ${env.DockerHubUser} -p ${env.DockerHubPass} "
-  sh "sudo docker image tag ${img}:latest ${env.DockerHubUser}/${img}:latest "
-  sh "sudo docker push ${env.DockerHubUser}/${img}:latest"
+  sh "sudo docker login -u ${DockerHubUser} -p ${DockerHubPass} "
+  sh "sudo docker image tag ${img}:latest ${DockerHubUser}/${img}:latest "
+  sh "sudo docker push ${DockerHubUser}/${img}:latest"
   }
   echo "successfuly pushed"
 }
